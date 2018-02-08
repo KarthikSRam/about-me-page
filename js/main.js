e3c84790747067ad92094756c3c34088ad716d7e
KarthikSRam <karthiksram100@gmail.com>
2018-02-08 12:23:47 -0800

    Added nav highlighting

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(window).on('load', function(){
   console.log("Sanity check");
+
+  //Functionality for highlighting the current nav section.
+  //Referenced from https://codepen.io/clokey2k/pen/jgfFD
+  var $sections = $('.spacing-for-nav');
+  $(window).on("scroll", function(){
+    var currentScroll = $(this).scrollTop();
+
+    $sections.each(function(){
+      var divPosition = $(this).offset().top;
+      if(divPosition - 1 < currentScroll){
+        $currentSection = $(this);
+      }
+      var id = $currentSection.attr('id');
+      $('a').removeClass('active');
+      $("#"+id+"Link").addClass('active');
+    })
+  })
 })
